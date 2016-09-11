1493320e2401e1c37cb3fceb4b6610f6816c134f
lucasdobsonRMIT <s3449291@student.rmit.edu.au>
2016-09-11 15:54:35 +1000

    updated version with roles functionality almost finished

--- config/app.php
+++ config/app.php
@@ -156,7 +156,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        Zizaco\Entrust\EntrustServiceProvider::class,
     ],
 
     /*
@@ -204,6 +204,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Entrust' => Zizaco\Entrust\EntrustFacade::class,
 
     ],
 
