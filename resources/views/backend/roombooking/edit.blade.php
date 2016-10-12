e57747b60e87511221639fd9be7adc6582ff6a2b
stamalex12 <StamAlex1@Alexandross-MacBook-Pro.local>
2016-10-12 16:05:17 +1100

    Fixed rooms dropdown

--- resources/views/backend/roombooking/edit.blade.php
+++ resources/views/backend/roombooking/edit.blade.php
@@ -1,46 +1,75 @@
 @extends('layouts.master')
 
 @section('content')
+
+
     <div class="row">
-        <div class="col-md-12">
-            <h1>Edit Room</h1>
-            {!! Form::model($room, ['method' => 'PATCH', 'action' => ['Admin\RoomController@update', $room->id]]) !!}
-            <div class="form-group">
-                {!! Form::label('name', 'Name:') !!}
-                {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Name']) !!}
-            </div>
+        <div class="col-lg-6">
 
-            <div class="form-group">
-                {!! Form::label('price', 'Price:(AUD)') !!}
-                {!! Form::text('price', null, ['class' => 'form-control', 'placeholder' => 'Price']) !!}
-            </div>
-
-
-            <div class="form-group">
-                {!! Form::label('type', 'Type:') !!}
-                {!! Form::text('type', null, ['class' => 'form-control', 'placeholder' => 'Type']) !!}
-            </div>
-
-            <div class="form-group">
-                {!! Form::label('description', 'Description:') !!}
-                {!! Form::textarea('description', null, ['class' => 'form-control', 'placeholder' => 'Description']) !!}
-            </div>
-
-
-            {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
-            <a href="../type" class="btn btn-default btn-raised">Cancel</a>
-
-            {!! Form::close() !!}
-
-            @if ($errors->any())
-                <ul class="alert alert-danger">
-                    @foreach($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
+            @if($errors)
+                @foreach($errors->all() as $error)
+                    <p>{{ $error }}</p>
+                @endforeach
             @endif
 
-            <div class="divider"></div>
+            <form action="{{ url('admin/roombooking/' . $roombooking->id) }}" method="POST">
+                {{ csrf_field() }}
+                <input type="hidden" name="_method" value="PUT" />
+                <div class="form-group ">
+                    <label for="firstName"> First Name</label>
+                    <input type="text" class="form-control" name="firstName" value="{{ $roombooking->firstName }}" >
+                    @if ($errors->has('firstName'))
+                        <p class="help-block"><span class="glyphicon glyphicon-exclamation-sign"></span>
+                            {{ $errors->first('firstName') }}
+                        </p>
+                    @endif
+                </div>
+
+                <div class="form-group ">
+                    <label for="lastName"> Last Name</label>
+                    <input type="text" class="form-control" name="lastName" value="{{ $roombooking->lastName }}" >
+                    @if ($errors->has('lastName'))
+                        <p class="help-block"><span class="glyphicon glyphicon-exclamation-sign"></span>
+                            {{ $errors->first('lastName') }}
+                        </p>
+                    @endif
+                </div>
+
+
+                <div class="form-group ">
+                    <label for="time">Time</label>
+                    <div class="input-group">
+                        <input type="text" class="form-control" name="time" value="{{ $roombooking->startDate . ' - ' . $roombooking->endDate }}" placeholder="Select your time">
+                        <span class="input-group-addon">
+						<span class="glyphicon glyphicon-calendar"></span>
+                    </span>
+                    </div>
+                    @if ($errors->has('time'))
+                        <p class="help-block"><span class="glyphicon glyphicon-exclamation-sign"></span>
+                            {{ $errors->first('time') }}
+                        </p>
+                    @endif
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
         </div>
     </div>
+@endsection
+
+@section('js')
+    <script src="{{URL::asset('js/daterangepicker.js')}}"></script>
+    <script type="text/javascript">
+        $(function () {
+            $('input[name="time"]').daterangepicker({
+                "timePicker": true,
+                "timePicker24Hour": true,
+                "timePickerIncrement": 15,
+                "autoApply": true,
+                "locale": {
+                    "format": "DD/MM/YYYY HH:mm:ss",
+                    "separator": " - ",
+                }
+            });
+        });
+    </script>
 @endsection
