2d104679b99e59a012cdd9fdcf08080f2364561f
Liam Mahony <splinky83@gmail.com>
2016-10-14 07:14:31 +1100

    Email sent when Application approved
    User is automatically sent an email when application approved.

--- app/Http/Controllers/Admin/AdminApplicationsController.php
+++ app/Http/Controllers/Admin/AdminApplicationsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Application;
+use App\User;
 use App\VolunteeringNeed;
 use App\Http\Requests;
 use App\Http\Controllers\Auth;
@@ -11,7 +12,11 @@ use Intervention\Image\Facades\Image;
 use Intervention\Image\File;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 use App\Role;
-use App\User;
+use App\Http\Requests\CreateEmailRequest;
+use App\Http\Requests\CreateEmailGroupRequest;
+use App\Email;
+use App\WebsiteInfo;
+use Illuminate\Support\Facades\Mail;
 
 class AdminApplicationsController extends Controller
 {
@@ -30,10 +35,22 @@ class AdminApplicationsController extends Controller
     public function approve($id)
     {
         $application = application::findorfail($id);
+        $user = user::findorfail($application->user_id);
         $application->status = 'Approved';
         $application->save();
 
         $applications = Application::all();
+
+        $data = ['email' => 'splinkye83@gmail.com','subject' => 'Application Approved', 'bodyMessage' => 'Your Application hass been approved.'];
+
+        $info = WebsiteInfo::all()->first();
+        Mail::send('emails.emailTemplate', $data, function ($message) use ($data, $info)
+        {
+            $message->from($info->email, $info->companyName);
+
+            $message->to($data['email'])->subject($data['subject']);
+        });
+
         return view('backend.applications.index', compact('applications'));
 
     }
