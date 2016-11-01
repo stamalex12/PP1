c0f299c891e17d429ce5603188f4e2fee3998749
lucasdobsonRMIT <s3449291@student.rmit.edu.au>
2016-11-01 22:19:05 +1100

    fixed register User problem

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -73,6 +73,7 @@ class AuthController extends Controller
             'username' => $data['username'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
+            'subscriber' => 1
         ]);
 
         $role = Role::where('name', '=', 'Visitor')->get();
